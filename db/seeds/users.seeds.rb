615eaa3d7e137c973597e4502e04648da6a67ddd
Pedro Bernardes <phec06@gmail.com>
2016-09-23 17:19:20 -0300

    Preventing seeds to be run on production

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,3 +1,5 @@
+raise "Don't seed in production!" if Rails.env.production?
+
 [:user_ray, :user_steve].each do |user_factory|
   unless User.exists?(email: FactoryGirl.attributes_for(user_factory)[:email])
     FactoryGirl.create user_factory
